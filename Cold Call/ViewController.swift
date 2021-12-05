08c51f5d99e12ecb10f40a6d58afb1e457ea404e
RAM111111 <r.r.99@windowslive.com>
2021-12-05 12:36:57 +0300

    first commit

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let names = [
+    "Ramia",
+    "wjdan",
+    "ahad",
+    "amaal",
+    "nora"
+    ]
+    @IBOutlet weak var labelnames: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeName(_ sender: UIButton) {
+        labelnames.text = names.randomElement()
+    }
+    
 }
 
